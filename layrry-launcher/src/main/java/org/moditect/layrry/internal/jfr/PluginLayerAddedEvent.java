71c760d34bb5abddb7b1d6d6b18c344a255604ef
Gunnar Morling <gunnar.morling@googlemail.com>
2020-05-01 18:11:51 +0200

    #7 JFR events on layer addition/removal

---
+++ layrry-launcher/src/main/java/org/moditect/layrry/internal/jfr/PluginLayerAddedEvent.java
@@ -0,0 +1,39 @@
+/**
+ *  Copyright 2020 The ModiTect authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.moditect.layrry.internal.jfr;
+
+import jdk.jfr.Category;
+import jdk.jfr.Description;
+import jdk.jfr.Event;
+import jdk.jfr.Label;
+import jdk.jfr.Name;
+import jdk.jfr.StackTrace;
+
+@Name(PluginLayerAddedEvent.NAME)
+@Label("Plug-in Layer Added")
+@Category("Layrry")
+@Description("A plug-in layer was added")
+@StackTrace(false)
+public class PluginLayerAddedEvent extends Event {
+
+    static final String NAME = "org.moditect.layrry.PluginLayerAdded";
+
+    @Label("Layer Name")
+    public String name;
+
+    @Label("Modules")
+    public String modules;
+}
