565b61842d6f8a087f065deeb4b0b543f3e078e5
Andres Almiray <aalmiray@gmail.com>
2020-09-27 21:11:16 +0200

    Detect OS and replace placeholders - Uses os-maven-plugin API to detect OS variables - Uses Mustache as template format

--- layrry-config/src/main/java/org/moditect/layrry/config/LayersConfigLoader.java
+++ layrry-config/src/main/java/org/moditect/layrry/config/LayersConfigLoader.java
@@ -15,9 +15,19 @@
  */
 package org.moditect.layrry.config;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import kr.motd.maven.os.Detector;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Properties;
 import java.util.ServiceLoader;
 
 public class LayersConfigLoader {
@@ -28,9 +38,8 @@ public class LayersConfigLoader {
         for (LayersConfigParser parser : parsers) {
             if (parser.supports(layersConfigFile)) {
                 try (InputStream inputStream = layersConfigFile.toUri().toURL().openStream()) {
-                    return parser.parse(inputStream);
-                }
-                catch (IOException e) {
+                    return parser.parse(replacePlaceholders(inputStream));
+                } catch (IOException e) {
                     throw new IllegalArgumentException("Unexpected error parsing config file. " + layersConfigFile, e);
                 }
             }
@@ -38,4 +47,66 @@ public class LayersConfigLoader {
         throw new IllegalArgumentException("Unsupported config format. " + layersConfigFile);
     }
 
+    private static InputStream replacePlaceholders(InputStream inputStream) {
+        // detect OS
+        OsDetector detector = new OsDetector();
+
+        Properties props = new Properties();
+        props.putAll(System.getProperties());
+        props.putAll(detector.getProperties());
+        // special case for JavaFX OS classifier
+        String javafxClassifier = resolveJavaFxClassifier(detector.get(Detector.DETECTED_NAME));
+        if (null != javafxClassifier) {
+            props.put("javafx.os.classifier", javafxClassifier);
+        }
+
+        // evaluate expressions
+        StringWriter input = new StringWriter();
+        MustacheFactory mf = new DefaultMustacheFactory();
+        Mustache mustache = mf.compile(new InputStreamReader(inputStream), "layrry");
+        mustache.execute(input, props);
+        input.flush();
+
+        return new ByteArrayInputStream(input.toString().getBytes());
+    }
+
+    private static String resolveJavaFxClassifier(String os) {
+        switch (os) {
+            case "linux":
+                return "linux";
+            case "windows":
+                return "windows";
+            case "osx":
+                return "mac";
+            default:
+                return null;
+        }
+    }
+
+    private static class OsDetector extends Detector {
+        private final Properties props = new Properties();
+
+        private OsDetector() {
+            props.put("failOnUnknownOS", "false");
+            detect(props, Collections.emptyList());
+        }
+
+        private Properties getProperties() {
+            return props;
+        }
+
+        private String get(String key) {
+            return props.getProperty(key);
+        }
+
+        @Override
+        protected void log(String message) {
+            // quiet
+        }
+
+        @Override
+        protected void logProperty(String name, String value) {
+            // quiet
+        }
+    }
 }
