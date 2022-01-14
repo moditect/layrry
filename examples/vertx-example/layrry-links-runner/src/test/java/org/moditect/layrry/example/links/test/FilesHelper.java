787c7db2eed01523474b1ee16e29cf6b418b2de0
Andres Almiray <aalmiray@gmail.com>
2022-01-14 10:31:32 +0100

    Update license headers

--- examples/vertx-example/layrry-links-runner/src/test/java/org/moditect/layrry/example/links/test/FilesHelper.java
+++ examples/vertx-example/layrry-links-runner/src/test/java/org/moditect/layrry/example/links/test/FilesHelper.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2020 The ModiTect authors
+ *  Copyright 2020 - 2022 The ModiTect authors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
